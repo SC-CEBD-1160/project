c89529a5ba0fa6f865514579acfde6b79615a411
Sebastien Church <schurch@groupeproaction.com>
2019-11-28 07:46:41 -0500

    Some new histogram graphs Cleanup project.py

--- project.py
+++ project.py
@@ -12,13 +12,11 @@ import pandas as pd
 from sklearn.datasets import load_wine
 from sklearn.metrics import f1_score
 import seaborn as sea
-import matplotlib.pyplot as plt
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
-from sklearn.linear_model import LogisticRegression
 
 sample_count = 50 #50
 range_count = 20 #20
@@ -57,10 +55,7 @@ def performance(lr, X_test, y_test):
 
 
 def removeColumn(df, column, index):
-    # print(f"--------------------- Dropping : {column}  -----------------------")
-
-    # df = pd.DataFrame(data=data['data'],
-    #                   columns=data['feature_names'])
+    # drop feature
     df.drop(column, axis=1, inplace=True)
     # print(f"Columns : {df.columns}")
     for j in range(0, range_count):
@@ -77,12 +72,6 @@ def removeColumn(df, column, index):
            print("Average Overall f1-score = ", average / sample_count)
            x[j][index] = (average / sample_count)
 
-           # print(x)
-
-    # performance(lr, X_test, y_test)
-    # visualize(df)
-
-
 def visualize(df):
     # Visualizing structure of dataset in 2D
     if df.count().size > 2:
@@ -93,16 +82,13 @@ def visualize(df):
         plt.show()
     plt.close()
 
-
-def reinitData(df):
-    # data = load_wine()
-    df = pd.DataFrame(data=data['data'],
-                      columns=data['feature_names'])
-
-
 # -----------------------------------------------
 # 1- Load wine dataset
 # -----------------------------------------------
+
+print("------------------------------------------------------------------")
+print(" Load wine dataset")
+print("------------------------------------------------------------------")
 
 raw_data = load_wine()
 df = pd.DataFrame(data=raw_data['data'],
@@ -113,60 +99,66 @@ data = df
 # -----------------------------------------------
 # 2- visualization of the wine dataset correlation
 # -----------------------------------------------
-# sea.set()
-#
-# fig, ax = plt.subplots(figsize=(20,20))
-# sea.heatmap(df.corr(), annot=True, cmap='summer')
-# ax.set_xticklabels(df.columns)
-# ax.set_yticklabels(df.columns)
-# b, t = plt.ylim() # discover the values for bottom and top
-# b += 0.5 # Add 0.5 to the bottom
-# t -= 0.5 # Subtract 0.5 from the top
-# plt.ylim(b, t) # update the ylim(bottom, top) values
-# plt.savefig('plots//wine_correlation_heatmap.png')
-#
-# plt.clf()
 
-# for i in raw_data['feature_names']:
-#     sea.distplot(data['alcohol'][data.target==i],
-#                  kde=1,label='{}'.format(i))
-#
-# plt.legend()
-# plt.show()
+print("------------------------------------------------------------------")
+print(" correlation heatmap")
+print("------------------------------------------------------------------")
+
+sea.set()
+
+fig, ax = plt.subplots(figsize=(20,20))
+sea.heatmap(df.corr(), annot=True, cmap='autumn')
+ax.set_xticklabels(df.columns)
+ax.set_yticklabels(df.columns)
+b, t = plt.ylim() # discover the values for bottom and top
+b += 0.5 # Add 0.5 to the bottom
+t -= 0.5 # Subtract 0.5 from the top
+plt.ylim(b, t) # update the ylim(bottom, top) values
+plt.savefig('plots//wine_correlation_heatmap.png')
+
+plt.clf()
+plt.close()
 
 # We'll use Seaborn's .kdeplot() method so we can cleanly distinguish each class per feature
+print("------------------------------------------------------------------")
+print(" kdeplot to show class distribution per feature")
+print("------------------------------------------------------------------")
 
-# features = pd.DataFrame(data=raw_data['data'],columns=raw_data['feature_names'])
-# data = features
-# data['target']=raw_data['target']
-# data['class']=data['target'].map(lambda ind: raw_data['target_names'][ind])
-# data.head()
-#
-# os.makedirs('plots/class_feature/', exist_ok=True)
-#
-# for feature in raw_data['feature_names']:
-#     print(feature)
-#     fig, ax = plt.subplots(figsize=(5, 5))
-#     gs1 = gridspec.GridSpec(3,1)
-#     ax1 = plt.subplot(gs1[:-1])
-#     ax2 = plt.subplot(gs1[-1])
-#     sea.boxplot(x=feature, y='class', data=data, ax=ax2)
-#     sea.kdeplot(data[feature][data.target==0],ax=ax1,label='0')
-#     sea.kdeplot(data[feature][data.target==1],ax=ax1,label='1')
-#     sea.kdeplot(data[feature][data.target==2],ax=ax1,label='2')
-#     ax2.yaxis.label.set_visible(False)
-#     ax1.xaxis.set_visible(False)
-#
-#     if feature == 'od280/od315_of_diluted_wines':
-#         plt.savefig(f'plots/class_feature/od280_class.png')
-#     else:
-#         plt.savefig(f'plots/class_feature/{feature}_class.png')
-#
-#     plt.close(fig)
+features = pd.DataFrame(data=raw_data['data'],columns=raw_data['feature_names'])
+data = features
+data['target']=raw_data['target']
+data['class']=data['target'].map(lambda ind: raw_data['target_names'][ind])
+data.head()
+
+os.makedirs('plots/class_feature/', exist_ok=True)
+
+for feature in raw_data['feature_names']:
+    print(feature)
+    fig, ax = plt.subplots(figsize=(5, 5))
+    gs1 = gridspec.GridSpec(3,1)
+    ax1 = plt.subplot(gs1[:-1])
+    ax2 = plt.subplot(gs1[-1])
+    sea.boxplot(x=feature, y='class', data=data, ax=ax2)
+    sea.kdeplot(data[feature][data.target==0],ax=ax1,label='0')
+    sea.kdeplot(data[feature][data.target==1],ax=ax1,label='1')
+    sea.kdeplot(data[feature][data.target==2],ax=ax1,label='2')
+    ax2.yaxis.label.set_visible(False)
+    ax1.xaxis.set_visible(False)
+
+    if feature == 'od280/od315_of_diluted_wines':
+        plt.savefig(f'plots/class_feature/od280_class.png')
+    else:
+        plt.savefig(f'plots/class_feature/{feature}_class.png')
+
+    plt.close(fig)
 
 # -----------------------------------------------
 # 3- cleanup of the wine dataset, extracting data
 # -----------------------------------------------
+
+print("------------------------------------------------------------------")
+print(" Extracting features that have low correlation")
+print("------------------------------------------------------------------")
 
 correlation_list = []
 positions = []
@@ -184,6 +176,7 @@ for x in df.corr().values:
         j = j + 1
     j = 0
     i = i + 1
+
 print("------------------------------------------------------------------")
 print(correlation_list)  # values
 print(positions)  # tuples
@@ -222,6 +215,7 @@ print(matches)
 # 3- classification regression on the wine dataset
 # -----------------------------------------------
 
+# Splitting features and target datasets into: train and test
 X = raw_data.data
 y = raw_data.target
 
@@ -229,6 +223,9 @@ data = raw_data
 
 # Splitting features and target datasets into: train and test
 # initial overall-f1 with no dropped feature
+print("------------------------------------------------------------------")
+print(" Initial overall calculation features (baseline all features)")
+print("------------------------------------------------------------------")
 all = []
 average = 0.0
 for i in range(0, range_count):
@@ -251,54 +248,46 @@ print("Total Average Overall f1-score = ", count / range_count)
     # performance(lr, X_test, y_test)
     # visualize(df)
 
-# for i in range(0, range_count):
-#     all[i] = max
-#
-# print(all)
-# visualize(df)
-
 # single removal of features
-# all_features = []
-# x = np.zeros((range_count, 13))
-# print("single removal of features")
-# count = 0
-# for column in matches:
-#    df = pd.DataFrame(data=data['data'],
-#                   columns=data['feature_names'])
-#    print("--------------------- Dropping feature : " + column[1])
-#    removeColumn(df, column[1], count)
-#    all_features.append(f'{column[1]}')
-#    count += 1
-#
-# single_removal_results = pd.DataFrame(data=x,
-#                        columns=all_features)
-#
-# print(single_removal_results)
-#
-# single_removal_results.insert(0,'All', all)
-#
-# tsint = single_removal_results.interpolate(method='cubic')
-#
-# fig, ax = plt.subplots(figsize=(20,10))
-# pal = sea.cubehelix_palette(8, rot=-.10, dark=.3)
-# ax.set_xticklabels(df.columns, rotation=45)
-# # Show each distribution with both violins and points
-# sea.violinplot(data=tsint, palette=pal, inner="points")
-#
-# os.makedirs('plots/Violinplots/', exist_ok=True)
-# plt.savefig(f'plots/Violinplots/Single_removal_features.png', dpi=300)
-#
-# fig, axes = plt.subplots(1, 1, figsize=(20, 10))
-# axes.plot(tsint)
-# axes.set_title('Single feature removal')
-# axes.set_xlabel('Sample #')
-# axes.set_ylabel('f1-score')
-# axes.legend(all_features)
-#
-# plt.savefig(f'plots/Single_removal_features.png', dpi=300)
+all_features = []
+x = np.zeros((range_count, 13))
+print("------------------------------------------------------------------")
+print(" Single removal of features")
+print("------------------------------------------------------------------")
+count = 0
+for column in matches:
+   df = pd.DataFrame(data=data['data'],
+                  columns=data['feature_names'])
+   print("--------------------- Dropping feature : " + column[1])
+   removeColumn(df, column[1], count)
+   all_features.append(f'{column[1]}')
+   count += 1
+
+single_removal_results = pd.DataFrame(data=x,
+                       columns=all_features)
+
+print(single_removal_results)
+
+single_removal_results.insert(0,'All', all)
+
+tsint = single_removal_results.interpolate(method='linear')
+
+print("------------------------------------------------------------------")
+print(" Generating violinplot for single removal")
+print("------------------------------------------------------------------")
+fig, ax = plt.subplots(figsize=(20,10))
+pal = sea.cubehelix_palette(8, rot=-.10, dark=.3)
+ax.set_xticklabels(df.columns, rotation=45)
+# Show each distribution with both violins and points
+sea.violinplot(data=tsint, palette=pal, inner="points")
+
+os.makedirs('plots/Violinplots/', exist_ok=True)
+plt.savefig(f'plots/Violinplots/Single_removal_features.png', dpi=300)
 
 # for feature in matches incremental removal of features
-# print("incremental removal of features")
+print("------------------------------------------------------------------")
+print(" Incremental removal of features")
+print("------------------------------------------------------------------")
 features = []
 x = np.zeros((range_count, 7))
 count = 0
@@ -319,8 +308,12 @@ incremental_removal_results.insert(0,'All', all)
 
 print(incremental_removal_results)
 
-tsint = incremental_removal_results.interpolate(method='cubic')
+tsint = incremental_removal_results.interpolate(method='linear')
 
+print("------------------------------------------------------------------")
+print(" Generating violinplot for incremental removal of features")
+print("------------------------------------------------------------------")
+# analysis of the incremental removal of features
 # Use violin plot to get a custom sequential palette
 fig, ax = plt.subplots(figsize=(10,10))
 pal = sea.cubehelix_palette(8, rot=-.7, dark=.3)
@@ -331,28 +324,44 @@ os.makedirs('plots/Violinplots/', exist_ok=True)
 plt.savefig(f'plots/Violinplots/Incremental_removal_features.png', dpi=300)
 plt.close()
 
-fig, axes = plt.subplots(1, 1, figsize=(20, 10))
-axes.plot(tsint)
-axes.set_title('Incremental feature removal')
-axes.set_xlabel('Sample #')
-axes.set_ylabel('f1-score')
-axes.legend(features)
-
-plt.savefig(f'plots/Incremental_removal_features.png', dpi=300)
-plt.close()
-
 # bar plot to show the mean value of each features
 mean = tsint.mean()
 print(mean)
 
+print("------------------------------------------------------------------")
+print("generating histogram plot from mean values")
+print("------------------------------------------------------------------")
+
 fig, axes = plt.subplots(1, 1, figsize=(5, 5))
-plt.ylim(0.91, 0.98)
+plt.ylim(0.91, 0.97)
 axes.set_xticklabels(tsint.columns, rotation=45)
-axes.set_title('Histogram mean feature removal')
+axes.set_title('Mean value of incremantal feature removal')
 axes.set_xlabel('Feature')
 axes.set_ylabel('f1-score')
 mean.plot.bar()
 
 plt.savefig(f'plots/Histogram_mean_values.png', dpi=300)
-
 plt.close()
+
+print("------------------------------------------------------------------")
+print("generating histogram plot for percentage variation")
+print("------------------------------------------------------------------")
+# getting percent difference between initial and subsequent features
+results = []
+for iter in range(0, 8):
+    results.append(((mean[iter] - mean[0]) / 2) * 100)
+
+print(results)
+
+# new dataframe from the results
+df=pd.DataFrame(data=results)
+print(df)
+
+fig, axes = plt.subplots(1, 1, figsize=(5, 5))
+axes.set_title('Percentage variation of incremental feature removal')
+axes.set_xlabel('Feature')
+axes.set_ylabel('percent variation')
+df.plot.bar()
+
+plt.savefig(f'plots/Histogram_percentage_variation_values.png', dpi=300)
+
